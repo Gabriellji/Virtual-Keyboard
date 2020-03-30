5e12841d89512e4c3059781f8837b49dd99d0ba0
Gabriellji <gabriellji@yahoo.com>
2020-03-30 16:39:38 +0300

    feat: add eslint-config-airbnb-base and add datacode to every button

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable camelcase */
+/* eslint-disable no-param-reassign */
+/* eslint-disable max-len */
+/* eslint-disable no-tabs */
+/* eslint-disable func-names */
 /* eslint-disable indent */
-const Keyboard =  {
+// eslint-disable-next-line strict
+const Keyboard = {
+	// eslint-disable-next-line no-tabs
 	elements: {
 		main: null,
 		keysContainer: null,
@@ -30,9 +39,9 @@ const Keyboard =  {
 
         // Add to DOM
         this.elements.main.appendChild(this.elements.keysContainer);
-        document.body.appendChild(this.elements.main); 
-        
-        // Automatically use keyboard for elements with .use-keyboard-input 
+        document.body.appendChild(this.elements.main);
+
+        // Automatically use keyboard for elements with .use-keyboard-input
         document.querySelectorAll(".use-keyboard-input").forEach(element => {
             element.addEventListener("focus", () => {
                 this.open(element.value, currentValue => {
@@ -40,34 +49,46 @@ const Keyboard =  {
                 });
             });
         });
-        
 	},
 
 	_createKeys() {
         const fragment = document.createDocumentFragment();
         const keyLayout = [
-            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-            "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-            "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
-            "space"
+            "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspase",
+            "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\", "DEL",
+            "Caps Lock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "`", "ENTER",
+            "Shift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "↑", "Shift",
+            "Ctrl", "Win", "Alt", "space", "Alt", "Ctrl", "←", "↓", "→"
         ];
+
+        const dataCode = [
+            "Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspase",
+            "Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash", "Delete",
+            "CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter",
+            "ShiftLeft", "Backslash", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ArrowUp", "ShiftRight",
+            "ControlLeft", "OSLeft", "AltLeft", "Space", "AltRight", "ControlRight", "ArrowLeft", "ArrowDown", "ArrowRight"
+          ];
+
 
         // Creates HTML for an icon
         const createIconHTML = (icon_name) => {
             return `<i class="material-icons">${icon_name}</i>`;
         };
 
-        keyLayout.forEach(key => {
+        keyLayout.forEach((key, index) => {
             const keyElement = document.createElement("button");
-            const insertLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+            const insertLineBreak = ["Backspase", "DEL", "ENTER", "Ctrl"].indexOf(key) !== -1;
 
             // Add attributes/classes
+
+            keyElement.setAttribute("data-code", dataCode[index]);
+            keyElement.innerText = keyLayout[index];
+
             keyElement.setAttribute("type", "button");
             keyElement.classList.add("keyboard__key");
 
             switch (key) {
-                case "backspace":
+                case "Backspase":
                     keyElement.classList.add("keyboard__key--wide");
                     keyElement.innerHTML = createIconHTML("backspace");
 
@@ -78,7 +99,7 @@ const Keyboard =  {
 
                     break;
 
-                case "caps":
+                case "Caps Lock":
                     keyElement.classList.add("keyboard__key--wide", "keyboard__key--activatable");
                     keyElement.innerHTML = createIconHTML("keyboard_capslock");
 
@@ -89,50 +110,50 @@ const Keyboard =  {
 
                     break;
 
-                case "enter":
+                case "ENTER":
                     keyElement.classList.add("keyboard__key--wide");
                     keyElement.innerHTML = createIconHTML("keyboard_return");
-    
+
                     keyElement.addEventListener("click", () => {
                         this.properties.value += "\n";
                         this._triggerEvent("oninput");
                     });
-    
-                    break;  
+
+                    break;
                 case "space":
                     keyElement.classList.add("keyboard__key--extra-wide");
                     keyElement.innerHTML = createIconHTML("space_bar");
-    
+
                     keyElement.addEventListener("click", () => {
                         this.properties.value += " ";
                         this._triggerEvent("oninput");
                     });
-    
+
                     break;
                 case "done":
                     keyElement.classList.add("keyboard__key--wide", "keyboard__key--dark");
                     keyElement.innerHTML = createIconHTML("scheck_circle");
-    
+
                     keyElement.addEventListener("click", () => {
                         this.close();
                         this._triggerEvent("onclose");
                     });
-    
-                    break; 
+
+                    break;
                 default:
                         keyElement.textContent = key.toLowerCase();
-        
+
                         keyElement.addEventListener("click", () => {
                             this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
                             this._triggerEvent("oninput");
                         });
-        
-                    break;          
+
+                    break;
             }
 
             fragment.appendChild(keyElement);
 
-            if(insertLineBreak) {
+            if (insertLineBreak) {
                 fragment.appendChild(document.createElement("br"));
             }
         });
@@ -140,12 +161,12 @@ const Keyboard =  {
         return fragment;
 	},
 
-	_triggerEvent (handlerName) {
-		if (typeof this.eventHandlers[handlerName] == "function") {
+	_triggerEvent(handlerName) {
+		if (typeof this.eventHandlers[handlerName] === "function") {
             this.eventHandlers[handlerName](this.properties.value);
         }
 	},
-    
+
 	_toggleCapsLock() {
         this.properties.capsLock = !this.properties.capsLock;
 
@@ -154,7 +175,6 @@ const Keyboard =  {
                 key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
             }
         }
-		
 	},
 
 	open(initialValue, oninput, onclose) {
@@ -169,11 +189,9 @@ const Keyboard =  {
         this.eventHandlers.oninput = oninput;
         this.eventHandlers.onclose = onclose;
         this.elements.main.classList.add("keyboard--hidden");
-
 	}
 };
 
 window.addEventListener("DOMContentLoaded", function () {
     Keyboard.init();
-
 });
